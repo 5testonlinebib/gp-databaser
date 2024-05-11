dd61dc905a14410682f2fc07522437986bd44f03
Joneboef <jonas@godepenge.dk>
2024-05-11 16:22:08 +0200

    Update 5.js

--- testDatabaser/5/5.js
+++ testDatabaser/5/5.js
@@ -67,11 +67,10 @@ var database = [
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
     },
   {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
+    
         name: 'Where does money come from? <br> A guide to the UK monetary and banking system ', //navn
         type: 'Bog', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
-        rating: 8, //kvaliteten, tal mellem 1-10
+        rating: 9, //kvaliteten, tal mellem 1-10
         price: 100.00, //Pris for at tilgå referencen
         date: '2011-01-01', //tilblivesesdato for det man referer til, år-månded-dag
         authors: 'Josh-Ryan Collins, Tony Greenham, Richard Werner, Andrew Jackson', //forfatter eller forfattere
@@ -79,5 +78,17 @@ var database = [
         description: 'En saglig introduktion til pengeskabelse på akademisk niveau. Uden normative vurderinger, men meget solid deskriptiv forklaring af pengeskabelsen. Passende til brug for bachelor eller specialeskriving om emnet ', //Beskrivelse af den specifikke Artikel, bog osv.
         link: "https://neweconomics.org/2012/12/where-does-money-come-from", //link 
         image: "https://neweconomics.org/uploads/images/2012/12/WDMCF2.png", //link til billede
+    }, {
+        
+        name: 'Modernising Money: Why Our Monetary System is Broken, and How We Fix It', //navn
+        type: 'Bog', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
+        rating: 9, //kvaliteten, tal mellem 1-10
+        price: 0.00, //Pris for at tilgå referencen
+        date: '2012-01-01', //tilblivesesdato for det man referer til, år-månded-dag
+        authors: 'Andrew Jackson, Ben Dyson', //forfatter eller forfattere
+        theme: ['Pengeskabelse', 'Pengereform', 'Finanskrise','Centralbanker','Pengepolitik', 'English' ], //Emnerne på denne form ['Emne1', 'Emne2', 'Emne3']. Det skal være med stort forbogstav
+        description: 'Modernising Money redegør for forfatternes løsning på finanskrisen. Løsningen, selvom den er vidtrækkende, behandles ikke som utopisk, men som et praktisk forslag: overgangen fra det nuværende pengesystem beskrives i detaljer. Forfatterne behandler spørgsmålet om, hvordan vi kan forbedre penge og banksystemet så det fungerer for befolkningen som helhed. Første udgivelse af Positive Money (Gode Penge i England) 
+        link: "hhttps://bibliotek.dk/materiale/modernising-money_andrew-jackson/work-of%3A800010-katalog%3A99122060680705763?type=bog", //link 
+        image: "https://positivemoney.org/wp-content/uploads/2013/08/Modernising-Money-Cover-Web-300px-203x300.jpg", //link til billede
     },
     ];
